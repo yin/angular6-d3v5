72a8b656323496080b98c0a6089134805f57a60a
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2018-07-09 17:03:20 +0200

    Added D3 Chord diagram and it LIVES!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,130 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngAfterContentInit() {
+    var matrixData={
+      "Occupants":
+      [0, 10,10,10,10,0,0,0,10,10,10,10,10],
+      Flooring:
+      [70,0,0,0,0,0,30,0,0,0,0,0,0],
+      Millwork:
+      [40, 0,0,0,0,0,60,0,0,0,0,0],
+      Ceilings:
+      [60, 0,0,0,0,0,40,0,0,0,0,0,0],
+      "Wet Applied Products":
+      [60, 0,0,0,0,0,40,0,0,0,0,0,0],
+      "Curtain Wall":
+      [0, 0,0,0,0,0,100,0,0,0,0,0,0],
+      "Environment":
+      [0, 10,10,10,10,0,10,0,10,10,10,0,10],
+      Roofing:
+      [0, 0,0,0,0,0,100,0,0,0,0,0,0],
+      Insulation:
+      [50, 0,0,0,0,0,50,0,0,0,0,0,0],
+      MEPFP:
+      [40, 0,0,0,0,0,60,0,0,0,0,0,0],
+      Structure:
+      [50, 0,0,0,0,0,50,0,0,0,0,0,0],
+      "Furniture":
+      [90, 0,0,0,0,0,10,0,0,0,0,0,0]
+    };
+
+    var matrix= Object.values(matrixData);
+    var matrixKeys = Object.keys(matrixData);
+
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        outerRadius = Math.min(width, height) * 0.5 - 150,
+        innerRadius = outerRadius - 30;
+
+    var formatValue = d3.formatPrefix(",.0", 1e3);
+
+    var chord = d3.chord()
+        .padAngle(0.024)
+        .sortSubgroups(d3.descending);
+
+    var arc = d3.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius);
+
+    var ribbon = d3.ribbon()
+        .radius(innerRadius);
+
+    var color = d3.scaleOrdinal()
+        .domain(d3.range(4))
+        .range(["#6FCDE3",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#E5E52B",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#D7DAE5",
+                "#D7DAE5"
+               ]);
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ") rotate(75)")
+        .datum(chord(matrix));
+
+    var group = g.append("g")
+        .attr("class", "groups")
+      .selectAll("g")
+      .data(function(chords) { return chords.groups; })
+      .enter().append("g");
+
+    group.append("path")
+        .style("fill", function(d) { return color(d.index); })
+        .style("stroke", function(d) { return d3.rgb(color(d.index)).darker(); })
+        .attr("d", arc);
+
+    var groupTick = group.selectAll(".group-tick")
+      .data(function(d) { return groupTicks(d, 1e3); })
+      .enter().append("g")
+        .attr("class", "group-tick")
+        .attr("transform", function(d) {
+          return "rotate(" + (d.angle * 180 / Math.PI - 75) +
+            ") translate(" + outerRadius + ",2)";
+        });
+
+    groupTick.append("line")
+        .attr("x2", 6);
+
+    groupTick
+      .filter(function(d) { return d.value % 5e3 === 0; })
+      .append("text")
+        .attr("x", 8)
+        .attr("dy", ".35em")
+        .attr("transform", function(d) { return d.angle > Math.PI/2 && d.angle < Math.PI*3/2 ? "rotate(180) translate(-16)" : null; })
+        .style("text-anchor", function(d) { return d.angle > Math.PI/2 && d.angle < Math.PI*3/2? "end" : null; })
+        .text(function(d) {
+      return matrixKeys[d.index];
+    });
+
+    g.append("g")
+        .attr("class", "ribbons")
+      .selectAll("path")
+      .data(function(chords) { return chords; })
+      .enter().append("path")
+        .attr("d", ribbon)
+        .style("fill", function(d) { return color(d.target.index); })
+        .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
+
+    // Returns an array of tick angles and values for a given group and step.
+    function groupTicks(d, step) {
+      var k = (d.endAngle - d.startAngle) / d.value;
+      return d3.range(0, d.value, step).map(function(value) {
+        return {
+          index:d.index,
+          value: value,
+          angle: value * k + d.startAngle
+        };
+      });
+    }
+  }
 }
